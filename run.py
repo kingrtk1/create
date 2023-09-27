49165c1345434a3bc43bfbb859f84fca710f5d4e
kingrtk1 <108303178+kingrtk1@users.noreply.github.com>
2023-09-28 01:14:57 +0600

    Update run.py

--- run.py
+++ run.py
@@ -110,23 +110,6 @@ try:
     c.close()
     body = buffer.getvalue().decode('utf-8')
     response = body
-    if "Version-1.5" in response:
-        print(" Already Up To Date...")
-        linex()
-        print(' Checking For Approval....')
-        linex()
-        if kex in response:
-            print('\033[1;32m Your Key is Approved....\033[1;37m');time.sleep(2)
-        elif "Trail-30min" in response:
-            print('\033[1;36m Enjoy Free Trial ....\033[1;37m');time.sleep(2)
-        else:
-            print("\033[1;31m Read note first! ")
-            linex()
-            print(" You cannot run this tool without AKING permeations \n Note: Agar Tool mai Koi Masla Aata HA To Me Jald Se Jald Try Kronga Fix Krne Ke Agr Nhi Hota To May Be kujh time lag jya fix krne me!\n payment krne ka bad return nhi hoge agr buy krna ha to ok else skip,exit")
-            linex()
-            print(" Your key: "+fkeyx);subprocess.check_output(["am", "start", "https://api.whatsapp.com/send?phone=+923203714588&text="+(" Hi AKING sir i want Buy Auto Create Ids Tool Please Approve My Token\n Token:- "+ fkeyx)]);time.sleep(2)
-            exit('\n Run:  python create.py')
-            sys.exit()
     else:
         print(' Update done successfully wait for setup! ')
         time.sleep(2)
